b48af1338df6f0d4c1661c155ef724fd1a4d8c23
surabhai00007 <surabhai00007@gmail.com>
2025-08-14 13:04:23 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "22537641"))
 API_HASH = environ.get("API_HASH", "6c1eee32be959812f0598919209a2105")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8492211807:AAHbsGQREbjPHATmPiNog6ZgQxNk9aA_c9o")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8243267649:AAFlIRbe9PfgDpQwe4kSPC-4Qq4EpQ3J_hE")
 
 OWNER = int(environ.get("OWNER", "6748247898"))
 CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
